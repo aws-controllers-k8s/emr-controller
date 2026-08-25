db1aa455ba1bb99b20902479f4786bc557d14130
knottnt <trevor.knottnerus@gmail.com>
2026-08-25 15:03:33 -0700

    Configure terminal_codes

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -28,5 +28,5 @@ var (
 // time and are immutable for the life of the generated code.
 const (
 	ACKGenerateVersion   = "v0.62.1-4-gd4aaca5"
-	ACKGenerateBuildDate = "2026-08-25T20:00:13Z"
+	ACKGenerateBuildDate = "2026-08-25T22:01:52Z"
 )
