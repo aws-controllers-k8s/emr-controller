d65b629ac7689406afcd2addc407c2cf77e2ad66
knottnt <trevor.knottnerus@gmail.com>
2026-08-27 11:55:24 -0700

    Release artifacts for release v0.1.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -27,6 +27,6 @@ var (
 // that generated this controller's code. They are baked in at code-generation
 // time and are immutable for the life of the generated code.
 const (
-	ACKGenerateVersion   = "v0.62.1-4-gd4aaca5"
-	ACKGenerateBuildDate = "2026-08-27T00:07:43Z"
+	ACKGenerateVersion   = "v0.62.1-6-gb775ac9"
+	ACKGenerateBuildDate = "2026-08-27T18:54:00Z"
 )
